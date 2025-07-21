870a554e73c288be2201c87fa4c0850f9dd51ffc
Taira Aikawa <tir.3t.f22@gmail.com>
2025-07-21 04:06:35 +0000

    CI を修正しました

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,6 @@
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
-# your test database is "scratch space" for the test suite and is wipedconfig.hosts.clear
+# your test database is "scratch space" for the test suite and is wiped
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
