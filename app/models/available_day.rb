d9cedfa0066d28b827eb46f7332f3587f27ded1a
Taira Aikawa <tir.3t.f22@gmail.com>
2025-08-08 20:08:19 +0000

    AvailableDayのmodel テストの作成

--- app/models/available_day.rb
+++ app/models/available_day.rb
@@ -8,6 +8,6 @@
 #  updated_at :datetime         not null
 #
 class AvailableDay < ApplicationRecord
-  enum :name { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
+  enum :name, { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
   validates :name, presence: true, uniqueness: true
 end
--- spec/factories/available_days.rb
+++ spec/factories/available_days.rb
@@ -9,6 +9,14 @@
 #
 FactoryBot.define do
   factory :available_day do
-    name { 1 }
+    name { 0 } # デフォルトはSunday
+
+    trait :sunday             do name { 0 } end
+    trait :monday             do name { 1 } end
+    trait :tuesday            do name { 2 } end
+    trait :wednesday          do name { 3 } end
+    trait :thursday           do name { 4 } end
+    trait :friday             do name { 5 } end
+    trait :saturday           do name { 6 } end
   end
 end
