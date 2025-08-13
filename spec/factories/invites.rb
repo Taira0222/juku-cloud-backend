7c853815c6a5dd672163d18d27648ca3f9705532
Taira Aikawa <tir.3t.f22@gmail.com>
2025-08-13 06:46:56 +0000

    invites_controllerの作成と、services/generate_token のテスト追加

--- spec/factories/invites.rb
+++ spec/factories/invites.rb
@@ -2,21 +2,21 @@
 #
 # Table name: invites
 #
-#  id         :bigint           not null, primary key
-#  expires_at :datetime         not null
-#  max_uses   :integer          default(1), not null
-#  role       :integer          default("teacher"), not null
-#  token      :string           not null
-#  used_at    :datetime
-#  uses_count :integer          default(0), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  school_id  :bigint           not null
+#  id           :bigint           not null, primary key
+#  expires_at   :datetime         not null
+#  max_uses     :integer          default(1), not null
+#  role         :integer          default("teacher"), not null
+#  token_digest :string           not null
+#  used_at      :datetime
+#  uses_count   :integer          default(0), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  school_id    :bigint           not null
 #
 # Indexes
 #
-#  index_invites_on_school_id  (school_id)
-#  index_invites_on_token      (token) UNIQUE
+#  index_invites_on_school_id     (school_id)
+#  index_invites_on_token_digest  (token_digest) UNIQUE
 #
 # Foreign Keys
 #
@@ -25,11 +25,18 @@
 FactoryBot.define do
   factory :invite do
     association :school
-    token { SecureRandom.hex(10) }
     role { :teacher }
     max_uses { 1 }
     uses_count { 0 }
-    expires_at { Time.current + 7.days }
-    used_at { nil }
+    expires_at { 3.days.from_now }
+    # DBに保存されない一時的な属性
+    transient { raw_token { SecureRandom.hex(10) } }
+
+    token_digest { Invite.digest(raw_token) }
+
+    # テストで raw_token をinvite.raw_tokenのように使えるようにする。
+    after(:build) do |invite, evaluator|
+      invite.define_singleton_method(:raw_token) { evaluator.raw_token }
+    end
   end
 end
