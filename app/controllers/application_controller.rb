e0f1b6a400958792c7765fbbdd33ca021b59c631
Taira Aikawa <tir.3t.f22@gmail.com>
2025-08-14 18:14:15 +0000

    fix test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,17 @@ class ApplicationController < ActionController::API
 
   protected
 
-  # DeviseTokenAuth用の認証メソッドエイリアス
-  alias_method :current_user, :current_api_v1_user
-  alias_method :authenticate_user!, :authenticate_api_v1_user!
-  alias_method :user_signed_in?, :api_v1_user_signed_in?
+  def current_user
+    current_api_v1_user
+  end
+
+  def authenticate_user!
+    authenticate_api_v1_user!
+  end
+
+  def user_signed_in?
+    api_v1_user_signed_in?
+  end
 
   private
 
