e6a99607174fce3c766dc24f5f70c614a5683d9a
Taira Aikawa <tir.3t.f22@gmail.com>
2025-09-22 00:18:33 +0000

    student destroy のテスト作成

--- app/services/students/validator.rb
+++ app/services/students/validator.rb
@@ -2,7 +2,7 @@ module Students
   class Validator
     def self.call(id:)
       student = Student.find_by(id: id)
-      # studentが存在しない場合は404
+      # find_by で意図的に独自のエラーメッセージを返すようにしている
       unless student
         raise ActiveRecord::RecordNotFound, I18n.t("students.errors.not_found")
       end
