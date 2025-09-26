bc38e29973a66d64c99744a6ea39c5433e2a0eee
Taira Aikawa <tir.3t.f22@gmail.com>
2025-09-26 11:57:43 +0000

    student trait テーブルの不要なカラムを削除

--- app/models/student_trait.rb
+++ app/models/student_trait.rb
@@ -2,62 +2,28 @@
 #
 # Table name: student_traits
 #
-#  id                   :bigint           not null, primary key
-#  category             :integer
-#  created_by_name      :string           default(""), not null
-#  description          :text
-#  last_updated_by_name :string
-#  title                :string
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  created_by_id        :bigint
-#  last_updated_by_id   :bigint
-#  student_id           :bigint           not null
+#  id          :bigint           not null, primary key
+#  category    :integer
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  student_id  :bigint           not null
 #
 # Indexes
 #
-#  index_student_traits_on_created_by_id       (created_by_id)
-#  index_student_traits_on_last_updated_by_id  (last_updated_by_id)
-#  index_student_traits_on_student_id          (student_id)
+#  index_student_traits_on_student_id  (student_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (created_by_id => users.id) ON DELETE => nullify
-#  fk_rails_...  (last_updated_by_id => users.id) ON DELETE => nullify
 #  fk_rails_...  (student_id => students.id)
 #
 class StudentTrait < ApplicationRecord
   belongs_to :student
-  # DBでは nullify だが、アプリケーション上では必須とする
-  belongs_to :created_by,
-             class_name: "User",
-             inverse_of: :student_traits_created,
-             optional: false
-  belongs_to :last_updated_by,
-             class_name: "User",
-             inverse_of: :student_traits_updated,
-             optional: true
 
   # good_category? メソッドが使用できるようになる
   enum :category, { good: 0, careful: 1 }, suffix: true
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 1000 }
-  # user.name の最大文字数に合わせる
-  validates :created_by_name, presence: true, length: { maximum: 50 }
-  validates :last_updated_by_name, length: { maximum: 50 }
-
-  # created_by_name は not null制約があるためbefore_validationでセットする
-  before_validation :snapshot_creator_name, on: :create
-  before_save :snapshot_updater_name
-
-  private
-
-  def snapshot_creator_name
-    self.created_by_name = created_by&.name if created_by_name.blank?
-  end
-
-  def snapshot_updater_name
-    self.last_updated_by_name = last_updated_by&.name
-  end
 end
--- spec/models/student_trait_spec.rb
+++ spec/models/student_trait_spec.rb
@@ -2,28 +2,20 @@
 #
 # Table name: student_traits
 #
-#  id                   :bigint           not null, primary key
-#  category             :integer
-#  created_by_name      :string           default(""), not null
-#  description          :text
-#  last_updated_by_name :string
-#  title                :string
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  created_by_id        :bigint
-#  last_updated_by_id   :bigint
-#  student_id           :bigint           not null
+#  id          :bigint           not null, primary key
+#  category    :integer
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  student_id  :bigint           not null
 #
 # Indexes
 #
-#  index_student_traits_on_created_by_id       (created_by_id)
-#  index_student_traits_on_last_updated_by_id  (last_updated_by_id)
-#  index_student_traits_on_student_id          (student_id)
+#  index_student_traits_on_student_id  (student_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (created_by_id => users.id) ON DELETE => nullify
-#  fk_rails_...  (last_updated_by_id => users.id) ON DELETE => nullify
 #  fk_rails_...  (student_id => students.id)
 #
 require "rails_helper"
@@ -69,24 +61,6 @@ RSpec.describe StudentTrait, type: :model do
     it "is not valid with enum category other than good and careful" do
       expect { student_trait.category = 2 }.to raise_error(ArgumentError)
     end
-
-    it "works snapshot_creator_name before_validation on create" do
-      student_trait.created_by_name = nil
-      student_trait.valid?
-      expect(student_trait.created_by_name).to eq student_trait.created_by.name
-    end
-
-    it "is valid even if a last_updated_by_name is nil" do
-      student_trait.last_updated_by_name = nil
-      expect(student_trait).to be_valid
-    end
-
-    it "works snapshot_updater_name before_save" do
-      updated_by = create(:user)
-      student_trait.last_updated_by = updated_by
-      student_trait.save!
-      expect(student_trait.last_updated_by_name).to eq updated_by.name
-    end
   end
   describe "association" do
     let(:association) { described_class.reflect_on_association(target) }
@@ -97,28 +71,6 @@ RSpec.describe StudentTrait, type: :model do
         expect(association.macro).to eq :belongs_to
         expect(association.options).to be_empty
         expect(association.class_name).to eq "Student"
-      end
-    end
-    context "created_by association" do
-      let(:target) { :created_by }
-      it "belongs to user" do
-        expect(association.macro).to eq :belongs_to
-        expect(association.options).to include(
-          inverse_of: :student_traits_created,
-          optional: false
-        )
-        expect(association.class_name).to eq "User"
-      end
-    end
-    context "last_updated_by association" do
-      let(:target) { :last_updated_by }
-      it "belongs to user" do
-        expect(association.macro).to eq :belongs_to
-        expect(association.options).to include(
-          inverse_of: :student_traits_updated,
-          optional: true
-        )
-        expect(association.class_name).to eq "User"
       end
     end
   end
